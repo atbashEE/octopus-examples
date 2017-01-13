6269ba1bd646fe724468b756e8b208fbf05a13b6
Rudy De Busscher <rdebusscher@gmail.com>
2017-01-13 20:34:20 +0100

    tests: Project with minimal amount of configuration

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Examples for the Octopus framework (JAVA EE Security framework)
 Contains the code examples which are created in the GitBook "Octopus cookbook".
 
 /ex1 [Simple JSF Application](https://rdebusscher.gitbooks.io/octopus-cookbook/content/chapter1.html)
+
+## tests directory
+
+Some additional projects for testing
+
+/minimal: The minimal configuration required to make Octopus run.
---
+++ tests/minimal/src/main/java/be/rubus/octopus/book/ex1/ApplicationSecurityData.java
@@ -0,0 +1,41 @@
+package be.rubus.octopus.book.ex1;
+
+import be.c4j.ee.security.realm.AuthenticationInfoBuilder;
+import be.c4j.ee.security.realm.SecurityDataProvider;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ */
+@ApplicationScoped
+public class ApplicationSecurityData implements SecurityDataProvider {
+
+    private int principalId = 0;
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) {
+        if (authenticationToken instanceof UsernamePasswordToken) {
+            UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
+
+            AuthenticationInfoBuilder authenticationInfoBuilder = new AuthenticationInfoBuilder();
+            authenticationInfoBuilder.principalId(principalId++).name(authenticationToken.getPrincipal().toString());
+            // TODO: Change for production. Here we use username as password
+            authenticationInfoBuilder.password(usernamePasswordToken.getUsername());
+
+            return authenticationInfoBuilder.build();
+        }
+
+        return null;
+    }
+
+    @Override
+    public AuthorizationInfo getAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+}

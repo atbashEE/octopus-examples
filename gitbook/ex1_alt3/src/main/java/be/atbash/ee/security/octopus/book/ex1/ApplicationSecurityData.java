bcdc54a0328e98c5027f1471a82f8e7504dca00d
Rudy De Busscher <rdebusscher@gmail.com>
2017-11-27 08:05:06 +0100

    Update ex1 alternatives (Atbash namespace, License info added)

--- gitbook/ex1_alt3/src/main/java/be/atbash/ee/security/octopus/book/ex1/ApplicationSecurityData.java
+++ gitbook/ex1_alt3/src/main/java/be/atbash/ee/security/octopus/book/ex1/ApplicationSecurityData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2017 Rudy De Busscher (www.c4j.be)
+ * Copyright 2017 Rudy De Busscher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-package be.rubus.octopus.book.ex1;
+package be.atbash.ee.security.octopus.book.ex1;
 
 import be.c4j.ee.security.realm.AuthenticationInfoBuilder;
 import be.c4j.ee.security.realm.SecurityDataProvider;
